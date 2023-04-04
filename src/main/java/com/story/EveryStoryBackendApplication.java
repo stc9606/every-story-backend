bfd7bbf92a9c920ec1b513cc60f69d984e12a673
scnoh <stc9606@naver.com>
2023-04-05 00:08:12 +0900

    init

---
+++ src/main/java/com/story/EveryStoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryStoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryStoryBackendApplication.class, args);
+	}
+
+}
